af14c8041512fdd801119264a610c3377e069abe
Mortaza Kayhan <kayhanmortaza@gmail.com>
2025-02-13 01:24:02 -0500

    Finished logic and Swing elements game is fully functional with hit and stay buttons  created

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -1,6 +1,6 @@
 import java.awt.*;
-import java.awt.event.*;
-import java.sql.SQLOutput;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
@@ -59,6 +59,36 @@ public class BlackJack {
             stayButton.setFocusable(false);
             buttonPanel.add(stayButton);
             frame.add(buttonPanel, BorderLayout.SOUTH);
+
+            hitButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    Card card = deck.remove(deck.size()-1);
+                    playerSum += card.getValue();
+                    playerAceCount += card.isAce()?1:0;
+                    playerHand.add(card);
+                    if(reducePlayerAce() > 21){
+                        hitButton.setEnabled(false);
+                    }
+                    gamePanel.repaint();
+                }
+            });
+
+            stayButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    hitButton.setEnabled(false);
+                    stayButton.setEnabled(false);
+
+                    while (dealerSum < 17){
+                        Card card = deck.remove(deck.size()-1);
+                        dealerSum += card.getValue();
+                        dealerAceCount += card.isAce()?1:0;
+                        dealerHand.add(card);
+                    }
+                    gamePanel.repaint();
+                }
+            });
+
+            gamePanel.repaint();
     }
 
     ArrayList<Card> deck; // Hey there's going be an ArrayList of type Card called deck(declaration)
@@ -85,21 +115,60 @@ public class BlackJack {
     JFrame frame = new JFrame("BlackJack");
     JPanel gamePanel = new JPanel() {
         @Override
-        public void paintComponent(Graphics g){
+        public void paintComponent(Graphics g) {
             super.paintComponent(g);
             try {
                 //draw hidden card
                 Image hiddenCardImg = new ImageIcon(getClass().getResource("./Cards/BACK.png")).getImage();
+                if (!stayButton.isEnabled()) {
+                    hiddenCardImg = new ImageIcon(getClass().getResource(hiddenCard.getImagePath())).getImage();
+                }
                 g.drawImage(hiddenCardImg, 20, 20, cardWidth, cardHeight, null);
 
                 //draw dealer's hand
-                for (int i = 0; i < dealerHand.size(); i++){
+                for (int i = 0; i < dealerHand.size(); i++) {
                     Card card = dealerHand.get(i);
                     Image cardImg = new ImageIcon(getClass().getResource(card.getImagePath())).getImage();
-                    g.drawImage(cardImg, cardWidth + 25 + (cardWidth + 5)*i, 20, cardWidth, cardHeight, null);
+                    g.drawImage(cardImg, cardWidth + 25 + (cardWidth + 5) * i, 20, cardWidth, cardHeight, null);
                 }
-            }catch(Exception e){
-                e.printStackTrace();
+
+                //draw player's hand
+                for (int i = 0; i < playerHand.size(); i++) {
+                    Card card = playerHand.get(i);
+                    Image cardImg = new ImageIcon(getClass().getResource(card.getImagePath())).getImage();
+                    g.drawImage(cardImg, 20 + (cardWidth + 5) * i, 320, cardWidth, cardHeight, null);
+                }
+
+                if (!stayButton.isEnabled()) {
+                    dealerSum = reduceDealerAce();
+                    playerSum = reducePlayerAce();
+                    System.out.println("STAY: ");
+                    System.out.println(dealerSum);
+                    System.out.println(playerSum);
+
+                    String message = "";
+                    if (playerSum > 21) {
+                        message = "You Lose!";
+                    } else if (dealerSum > 21) {
+                        message = "You Win!";
+                    }
+                    else if (playerSum == dealerSum){
+                        message = "Tie";
+                    }
+                    else if (playerSum > dealerSum){
+                        message = "You Win!";
+                    }
+                    else if (playerSum < dealerSum){
+                        message = "You Lose!";
+                    }
+
+                    g.setFont(new Font("Arial", Font.PLAIN, 30));
+                    g.setColor(Color.white);
+                    g.drawString(message, 220, 225);
+                }
+
+            } catch (Exception e){
+                    e.printStackTrace();
             }
         }
     };
@@ -182,5 +251,21 @@ public class BlackJack {
         }
         System.out.println("After Shuffle: ");
         System.out.println(deck);
+    }
+
+    public int reducePlayerAce(){
+        while (playerSum > 21 && playerAceCount > 0){
+            playerSum -= 10;
+            playerAceCount -= 1;
+        }
+        return playerSum;
+    }
+
+    public int reduceDealerAce(){
+        while (dealerSum > 21 && dealerAceCount > 0){
+            dealerSum -= 10;
+            dealerAceCount -= 1;
+        }
+        return dealerSum;
     }
 }
